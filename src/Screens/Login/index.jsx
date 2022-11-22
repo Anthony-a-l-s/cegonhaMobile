46dbc679c5ff4f6809c442919999daa30dd6ae56
ANTHONY LIMA E SILVA <72108893+Anthony-a-l-s@users.noreply.github.com>
2022-11-22 01:27:16 -0300

    init screen login

--- src/Screens/Login/index.jsx
+++ src/Screens/Login/index.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import {View, Text } from 'react-native';
- 
+import {
+    View,
+    Text,
+    SafeAreaView,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
+import {
+    ButtonStyles,
+    ComponentStyles,
+    ContainerStyles,
+    InputStyles,
+    TextStyles,
+    ViewStyles,
+} from '../../styles';
+import EmailIcon from 'react-native-vector-icons/EvilIcons';
 
-export default function Login(){
-    return(
-        <View>
-            <Text>Cegonha</Text>
-        </View>
+
+export default function Login() {
+    return (
+        <SafeAreaView style={ViewStyles.loginBackground}>
+            <View style={ViewStyles.ImputContainer}>
+                <EmailIcon name="trash" size={40} color="#040EEC" />
+                <TextInput
+                    style={InputStyles.imputLogin}
+                    underlineColorAndroid='transparent'
+                    placeholderTextColor={'#040EEC'}
+                />
+            </View>
+            <TextInput
+                style={InputStyles.imputLogin}
+                underlineColorAndroid='transparent'
+            />
+            <TouchableOpacity style={ButtonStyles.buttonLogin}>
+                <Text style={TextStyles.TextButton}>Entrar</Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
     )
 }
--- src/styles/GlobalStyles/ComponentStyles.js
+++ src/styles/GlobalStyles/ComponentStyles.js
@@ -1,29 +1,6 @@
-//Estilos de alguns Componentes para as páginas
 
 import {StyleSheet} from 'react-native';
 
 export const ComponentStyles = StyleSheet.create({
-  //Tela inicial - Dots
-  dotStyle: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    marginHorizontal: 3,
-    marginTop: 10,
-    backgroundColor: '#56C596',
-  },
-  inactiveDotStyle: {
-    backgroundColor: '#E0ECDE',
-  },
-  //
-  //Login Screen
-  EyeIcon: {
-    marginTop: '70%',
-    marginRight: 25,
-  },
-  //
-  checkboxIcon: {
-    marginTop: 10,
-    marginRight: 25,
-  },
+
 });
